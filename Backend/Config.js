8b2db6be6b0147b9ff1e7544015b0879568db70f
WILLIAM SOTO <wilsai1023gmail.com>
2023-02-27 11:40:30 -0500

    back

---
+++ Backend/Config.js
@@ -0,0 +1,36 @@
+let primerInput = document.getElementById("nombre")
+let segundoInput = document.getElementById("edad")
+let showName = document.getElementById("showName")
+let showHi = document.getElementById("showHi")
+let showPrivacy = document.getElementById("showPrivacy")
+let resultado = document.getElementById("resultado")
+
+let nombre = ""
+let edad = ""
+primerInput.addEventListener("input", (e) =>{
+    nombre = e.target.value
+})
+
+segundoInput.addEventListener("input", (e) =>{
+    edad = e.target.value
+})
+
+showName.addEventListener("click", () =>{
+    resultado.innerHTML = `<h1> Nombre: ${nombre}</h1>
+    <h2> Edad: ${edad}</h2>`
+})
+
+showHi.addEventListener("click", () =>{
+    resultado.innerHTML = `<h1> Hola ${nombre} Bienvenido</h1>`
+})
+
+showPrivacy.addEventListener("click", () =>{
+    if (edad > 18) {
+        resultado.innerHTML = `<h1> Hola ${nombre} Eres mayor de edad</h1>`
+    }else{
+        resultado.innerHTML = `<h1> Hola ${nombre} Eres mayor de edad</h1>`
+    }
+    
+})
+
+
