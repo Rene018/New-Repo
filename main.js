93ea10169c2bfd762360a4743591d84eef2281cd
WILLIAM SOTO <wilsai1023gmail.com>
2023-02-27 12:09:08 -0500

    back

--- main.js
+++ main.js
@@ -15,16 +15,19 @@ segundoInput.addEventListener("input", (e) =>{
     edad = e.target.value
 })
 
-showName.addEventListener("click", () =>{
+showName.addEventListener("click", (e) =>{
+    e.preventDefault()
     resultado.innerHTML = `<h1> Nombre: ${nombre}</h1>
     <h2> Edad: ${edad}</h2>`
 })
 
-showHi.addEventListener("click", () =>{
+showHi.addEventListener("click", (e) =>{
+    e.preventDefault()
     resultado.innerHTML = `<h1> Hola ${nombre} Bienvenido</h1>`
 })
 
-showPrivacy.addEventListener("click", () =>{
+showPrivacy.addEventListener("click", (e) =>{
+    e.preventDefault()
     if (edad > 18) {
         resultado.innerHTML = `<h1> Hola ${nombre} Eres mayor de edad</h1>`
     }else{
